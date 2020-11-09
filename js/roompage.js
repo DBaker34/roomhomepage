4da0dba5f81624a473a147747eb8c97add4999b2
Dbake34 <64293380+Dbake34@users.noreply.github.com>
2020-11-09 15:55:40 -0500

    Update roompage.js

--- js/roompage.js
+++ js/roompage.js
@@ -1,5 +1,30 @@
-const larrow = document.getElementById("larrow");
-const rarrow = document.getElementById("rarrow");
-const slide1 = document.getElementsByClassName("slide1");
-const slide2 = document.getElementsByClassName("slide2");
-const slide3 = document.getElementsByClassName("slide3");
+const slides = document.querySelectorAll(".slide");
+const imgs = document.querySelectorAll(".slideimg");
+const prevButton = document.getElementById("larrow");
+const nextButton = document.getElementById("rarrow");
+
+var currentSlide = 0;
+
+const nextSlide = function(){
+
+    slides[currentSlide].classList.remove("active");
+    imgs[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % 3;
+
+    slides[currentSlide].classList.add("active");
+    imgs[currentSlide].classList.add("active");
+};
+
+const prevSlide = function(){
+    slides[currentSlide].classList.remove("active");
+    imgs[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 2) % 3;
+
+    slides[currentSlide].classList.add("active");
+    imgs[currentSlide].classList.add("active");
+};
+
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
